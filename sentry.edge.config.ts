b2dd0b1345404ff9aa79c619805222ad4ceca124
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-06-21 23:07:50 +0800

    feat: integrate Sentry for error tracking and monitoring in Next.js application
    - Added Sentry as a dependency in package.json.
    - Created configuration files for Sentry on edge and server environments.
    - Implemented a sample API route to demonstrate error tracking with Sentry.
    - Developed a global error component to capture and report errors.
    - Created a sample frontend page to trigger and log errors to Sentry.
    - Configured client-side instrumentation for enhanced error tracking and session replay.
    - Established server-side instrumentation to handle request errors.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://14838123187f8e4bf37581de7f82525c@o4511387180007424.ingest.us.sentry.io/4511604009926656",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
