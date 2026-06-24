77dc9e02686674123a2e0d687c3482ecd7333011
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-06-24 15:13:53 +0800

    feat: Refactor UI components for improved readability and consistency
    - Updated PointerHighlight component to enhance readability and maintainability.
    - Refactored Popover component structure for better clarity and organization.
    - Improved Select component with consistent styling and structure.
    - Streamlined Table component for better code organization and readability.
    - Enhanced Textarea component with consistent styling.
    - Updated utility functions for improved code consistency.
    - Added ticket creation action with validation and error handling.
    - Introduced Alert component with variants for better user notifications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import Navbar from "@/components/navbar";
+
 import "./globals.css";
 
 const geistSans = Geist({
--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useActionState } from "react";
+import { createTicket } from "@/actions/ticket.actions";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -12,6 +18,11 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 
 function NewTicketPage() {
+	const [state, formAction] = useActionState(createTicket, {
+		success: false,
+		message: "",
+	});
+
 	return (
 		<main className="mx-auto flex h-dvh w-full max-w-5xl flex-col justify-center gap-3">
 			<section className="px-6 py-12">
@@ -27,45 +38,71 @@ function NewTicketPage() {
 
 				<div className="mt-12 min-w-xs md:min-w-2xl">
 					<Card>
-						<CardContent className="flex flex-col gap-6">
-							<div className="space-y-3">
-								<Label htmlFor="subject">Subject</Label>
-								<Input id="subject" placeholder="Enter subject" type="text" className="py-6" />
-							</div>
+						<CardContent>
+							<form action={formAction} className="flex flex-col gap-6">
+								<div className="space-y-3">
+									<Label htmlFor="subject">Subject</Label>
+									<Input
+										id="subject"
+										name="subject"
+										placeholder="Enter subject"
+										type="text"
+										className="py-6"
+										autoComplete="off"
+									/>
+								</div>
 
-							<div className="space-y-3">
-								<Label htmlFor="description">Description</Label>
+								<div className="space-y-3">
+									<Label htmlFor="description">Description</Label>
 
-								<Textarea
-									id="description"
-									placeholder="Enter the description"
-									className="field-sizing-content max-h-20 min-h-20 resize-none"
-								/>
-							</div>
+									<Textarea
+										id="description"
+										name="description"
+										placeholder="Enter the description"
+										className="field-sizing-content max-h-20 min-h-20 resize-none"
+									/>
+								</div>
 
-							<div className="space-y-3">
-								<Label htmlFor="priorityLevel">Priority Level</Label>
+								<div className="space-y-3">
+									<Label htmlFor="priorityLevel">Priority Level</Label>
 
-								<Select defaultValue="Low">
-									<SelectTrigger id="priorityLevel" className="w-full py-6">
-										<SelectValue placeholder="Select framework" />
-									</SelectTrigger>
+									<Select
+										defaultValue="Low"
+										onValueChange={(val) => console.log(val)}
+										name="priorityLevel"
+									>
+										<SelectTrigger id="priorityLevel" className="w-full py-6">
+											<SelectValue placeholder="Select framework" />
+										</SelectTrigger>
 
-									<SelectContent className="w-full p-3" position="popper">
-										<SelectItem value="Low">Low</SelectItem>
-										<SelectItem value="Medium">Medium</SelectItem>
-										<SelectItem value="High">High</SelectItem>
-									</SelectContent>
-								</Select>
-							</div>
+										<SelectContent className="w-full p-3" position="popper">
+											<SelectItem value="Low">Low</SelectItem>
+											<SelectItem value="Medium">Medium</SelectItem>
+											<SelectItem value="High">High</SelectItem>
+										</SelectContent>
+									</Select>
+								</div>
 
-							<div className="flex justify-end gap-1.5">
-								<Button size="lg" variant="ghost">
-									Clear
-								</Button>
+								{state.message && !state.success && (
+									<Alert className="border-destructive/80 bg-destructive/5 text-destructive w-full">
+										<AlertTitle>Error</AlertTitle>
 
-								<Button size="lg">Submit Ticket</Button>
-							</div>
+										<AlertDescription className="text-destructive/80">
+											<span className="first-letter:uppercase">{state.message}</span>
+										</AlertDescription>
+									</Alert>
+								)}
+
+								<div className="flex justify-end gap-1.5">
+									<Button size="lg" variant="ghost" type="reset">
+										Clear
+									</Button>
+
+									<Button size="lg" type="submit">
+										Submit Ticket
+									</Button>
+								</div>
+							</form>
 						</CardContent>
 					</Card>
 				</div>
